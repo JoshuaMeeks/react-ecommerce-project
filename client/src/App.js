9033b563e832dae2f50d2a40aa245e10fe2781ab
Joshua <jlmeeks587@gmail.com>
2023-05-23 18:29:18 -0400

    Add navbar with Bootstrap styling using Popper for dropdown functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,46 @@
-import data from './data.json';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import '@popperjs/core';
 
 function App() {
-  
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          {data.message}
-        </p>
-      </header>
-    </div>
+    <>
+      <nav className='navbar navbar-expand-lg bg-success'>
+        <div className='container'>
+          <a href='/home' className='navbar-brand'>
+            Logo
+          </a>
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a className='nav-link' href="/about">
+                About
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className='nav-link' href="/services">
+                Services
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className='nav-link' href="/contact">
+                Contact
+              </a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Dropdown
+              </a>
+              <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">Action</a></li>
+                <li><a class="dropdown-item" href="#">Another action</a></li>
+                <li><a class="dropdown-item" href="#">Something else here</a></li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </>
   );
 }
 
